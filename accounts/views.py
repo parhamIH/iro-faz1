c74e323f31f028bee9fb394e3e9f5c039448fb8c
parha,IH <parham.ilaghi@gmail.com>
2025-05-28 21:35:18 +0330

    ...

--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from django.contrib.auth import login
-
 
 from .models import Provider, Profile ,  CustomUser
 from .permissions import IsAdmin, IsProvider, IsAdminOrProvider

029386bdbc14a0c9a38f0497f0340dc272675bc1
parha,IH <parham.ilaghi@gmail.com>
2025-07-08 20:16:29 +0330

    hot fix urls

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     ProductViewSet, CategoryViewSet, BrandViewSet,
     ProductOptionViewSet, ColorViewSet,
-    SpecificationViewSet, ProductSpecificationViewSet, WarrantyViewSet
+    SpecificationViewSet, ProductSpecificationViewSet, WarrantyViewSet , TagViewSet
 )
 # لیست همه محصولات: http://localhost:8000/api/products/
 # یک محصول خاص: http://localhost:8000/api/products/[slug-محصول]/
@@ -20,6 +20,7 @@ router.register('colors', ColorViewSet)
 router.register('specifications', SpecificationViewSet)
 router.register('product-specifications', ProductSpecificationViewSet)
 router.register('warranties', WarrantyViewSet)
+router.register('tags', TagViewSet)
 # الگوهای URL
 urlpatterns = [
     # اضافه کردن تمام مسیرهای روتر به برنامه

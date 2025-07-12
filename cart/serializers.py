313b2a1b21d13f743878d2d42d2ea249d725b39d
parha,IH <parham.ilaghi@gmail.com>
2025-07-12 22:14:14 +0330

    cart changed

--- cart/serializers.py
+++ cart/serializers.py
@@ -2,34 +2,25 @@ from rest_framework import serializers
 from .models import Cart, CartItem, Order
 from store.models import ProductOption
 
+
 class ProductOptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductOption
         fields = ['id', 'option_price', 'quantity', 'color', 'product']
 
-<<<<<<< HEAD
-class CartItemSerializer(serializers.ModelSerializer):
-    package = ProductOptionSerializer(read_only=True)
-    package_id = serializers.PrimaryKeyRelatedField(queryset=ProductOption.objects.all(), source='package', write_only=True)
-=======
 
 class CartItemSerializer(serializers.ModelSerializer):
     package = ProductOptionSerializer(read_only=True)
     package_id = serializers.PrimaryKeyRelatedField(queryset=ProductOption.objects.all(), source='package', write_only=True)
     total_final_price = serializers.SerializerMethodField()
->>>>>>> 44326bdd00e41038f3f57ffbe53f1ba80f8e3880
 
     class Meta:
         model = CartItem
         fields = ['id', 'package', 'package_id', 'count', 'total_final_price']
-<<<<<<< HEAD
-from rest_framework import serializers
-=======
 
     def get_total_final_price(self, obj):
         return float(obj.total_final_price)
 
->>>>>>> 44326bdd00e41038f3f57ffbe53f1ba80f8e3880
 
 class CartSerializer(serializers.ModelSerializer):
     items = CartItemSerializer(source='cartitem_set', many=True, read_only=True)
@@ -42,12 +33,7 @@ class CartSerializer(serializers.ModelSerializer):
         read_only_fields = ['user', 'is_paid', 'created_date', 'updated_date', 'session_key']
 
     def get_total_price(self, obj):
-<<<<<<< HEAD
-        return obj.total_price()
-
-=======
         return float(obj.total_price())
->>>>>>> 44326bdd00e41038f3f57ffbe53f1ba80f8e3880
 
 
 class CartItemCreateSerializer(serializers.ModelSerializer):
@@ -57,10 +43,7 @@ class CartItemCreateSerializer(serializers.ModelSerializer):
         model = CartItem
         fields = ['id', 'package_id', 'count']
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 44326bdd00e41038f3f57ffbe53f1ba80f8e3880
 class OrderSerializer(serializers.ModelSerializer):
     cart = CartSerializer(read_only=True)
 
@@ -73,10 +56,7 @@ class OrderSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['user', 'order_number', 'order_date', 'total_price']
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 44326bdd00e41038f3f57ffbe53f1ba80f8e3880
 class OrderCreateSerializer(serializers.ModelSerializer):
     cart_id = serializers.PrimaryKeyRelatedField(queryset=Cart.objects.all(), source='cart')
 
